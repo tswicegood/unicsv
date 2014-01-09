ba29196a1abba682ce2d01cb90779061479e5ab9
Travis Swicegood <development@domain51.com>
2014-01-09 16:12:20 -0600

    Initial skeleton

---
+++ setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+import os
+
+# Stolen from django-registration
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('unicsv'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[len('unicsv/'):]
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
+
+
+setup(
+    name='unicsv',
+    version='0.1dev.0',
+    description='Package for dealing with unicode CSV files',
+    author='Travis Swicegood',
+    author_email='travis@domain51.com',
+    url='https://github.com/tswicegood/unicsv/',
+    packages=packages,
+    package_data={'unicsv': data_files},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
